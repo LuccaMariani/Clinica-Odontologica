55bb27ea15e0606399e76a1e798c314727a0e071
LuccaMariani <juanlucamariani@gmail.com>
2022-06-02 18:59:18 -0300

    mas commits del sprint 1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebaseConfig : {
+    apiKey: "AIzaSyCG8FYVFJiKt-rSYbEnABQ_sSC2956wFnU",
+    authDomain: "clinica-odontologica-mariani.firebaseapp.com",
+    projectId: "clinica-odontologica-mariani",
+    storageBucket: "clinica-odontologica-mariani.appspot.com",
+    messagingSenderId: "886119085873",
+    appId: "1:886119085873:web:c80f725f4a0e7952b62210",
+    measurementId: "G-SM0NP5PKTZ"
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCG8FYVFJiKt-rSYbEnABQ_sSC2956wFnU",
+    authDomain: "clinica-odontologica-mariani.firebaseapp.com",
+    projectId: "clinica-odontologica-mariani",
+    storageBucket: "clinica-odontologica-mariani.appspot.com",
+    messagingSenderId: "886119085873",
+    appId: "1:886119085873:web:c80f725f4a0e7952b62210",
+    measurementId: "G-SM0NP5PKTZ"
+  }
 };
 
 /*
